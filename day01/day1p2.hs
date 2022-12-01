b56e9bf7880fd457d291a9ad5a7d915f76984fee
Fredrik Furufors <furufors@gmail.com>
2022-12-01 06:19:17 +0100

    Added day 1

--- day01/day1p2.hs
+++ day01/day1p2.hs
@@ -1,4 +1,6 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.List.Split
+import Data.List
 main :: IO ()
-main = undefined
+main = interact $ show .sum. take 3. reverse . sort . map sum . map (map (read :: String -> Int)) . map lines . splitOn "\n\n"
