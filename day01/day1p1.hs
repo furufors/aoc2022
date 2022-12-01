661fa9d2287adeee486fff090193684374316e01
Fredrik Furufors <furufors@gmail.com>
2022-12-01 08:35:35 +0100

    Clean up day 1

--- day01/day1p1.hs
+++ day01/day1p1.hs
@@ -1,24 +1,6 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
-{-# LANGUAGE LambdaCase #-}
 import Data.List.Split
+
 main :: IO ()
 main = interact $ show . maximum . map sum . map (map (read :: String -> Int)) . map lines . splitOn "\n\n"
-
---toMax :: [[String]] -> Int
---toMax = max . map sum .
-
-{-
-\case { '.' -> e1; ...; .^. -> eN }
-main = interact $ show . sum . map (\case '(' -> 1; ')' -> -1; _ -> 0)
-main = interact $ show . (+1) . length . takeWhile (>= 0) . scanl1 (+) . map (\case '(' -> 1; ')' -> -1; _ -> 0)
-main = interact $ show . length . filter (id) . (\l -> zipWith (>) (drop 1 l) l) . map (\x -> read x :: Int) . lines
-
-main = interact $ show . trues . increasing . map tripletsum . triplets . parsein
-    where
-        trues = length . filter id
-        increasing l = zipWith (>) (drop 1 l) l
-        tripletsum (a,b,c) = a + b + c
-        triplets l = zip3 (drop 2 l) (drop 1 l) l
-        parsein = map (\x -> read x :: Int) . lines
--}
--- day01/day1p2.hs
+++ day01/day1p2.hs
@@ -2,5 +2,6 @@
 -- stack --resolver lts-18.18 script
 import Data.List.Split
 import Data.List
+
 main :: IO ()
 main = interact $ show .sum. take 3. reverse . sort . map sum . map (map (read :: String -> Int)) . map lines . splitOn "\n\n"
