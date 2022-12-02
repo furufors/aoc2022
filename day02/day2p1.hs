8748fa82fcfb5e283a159199db52f53fc7e17148
Fredrik Furufors <furufors@gmail.com>
2022-12-02 08:43:37 +0100

    Added day 2

--- day02/day2p1.hs
+++ day02/day2p1.hs
@@ -1,4 +1,35 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+data RPS = Rock | Paper | Scissors deriving Eq
+type Round = (RPS,RPS)
+
 main :: IO ()
-main = undefined
+main = interact $ show . sum . map  (score . parse) . lines
+
+parse :: String -> Round
+parse s = let x = head $ take 1 s
+              y = head . take 1 $ drop 2 s
+          in (elf x, player y)
+
+elf, player :: Char -> RPS
+elf 'A' = Rock
+elf 'B' = Paper
+elf 'C' = Scissors
+player    'X' = Rock
+player    'Y' = Paper
+player    'Z' = Scissors
+
+score :: Round -> Int
+score r = playScore r + roundScore r
+
+playScore :: Round -> Int
+playScore (_,Rock)     = 1
+playScore (_,Paper)    = 2
+playScore (_,Scissors) = 3
+
+roundScore :: Round -> Int
+roundScore (Rock,Paper)     = 6
+roundScore (Paper,Scissors) = 6
+roundScore (Scissors,Rock)  = 6
+roundScore (x,y) | x == y   = 3 -- Draw
+roundScore ________________ = 0
--- day02/day2p2.hs
+++ day02/day2p2.hs
@@ -1,4 +1,44 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+data RPS = Rock | Paper | Scissors deriving Eq
+data Outcome = Win | Draw | Lose
+type Round = (RPS,Outcome)
+
 main :: IO ()
-main = undefined
+main = interact $ show . sum . map  (score . parse) . lines
+
+parse :: String -> Round
+parse s = let x = head $ take 1 s
+              y = head . take 1 $ drop 2 s
+          in (elf x, outcome y)
+
+elf :: Char -> RPS
+elf 'A' = Rock
+elf 'B' = Paper
+elf 'C' = Scissors
+outcome :: Char -> Outcome
+outcome    'X' = Lose
+outcome    'Y' = Draw
+outcome    'Z' = Win
+
+score :: Round -> Int
+score r = playScore r + roundScore r
+
+playScore :: Round -> Int
+playScore (Rock, Win) = signScore Paper
+playScore (Paper, Win) = signScore Scissors
+playScore (Scissors, Win) = signScore Rock
+playScore (Rock, Lose) = signScore Scissors
+playScore (Paper, Lose) = signScore Rock
+playScore (Scissors, Lose) = signScore Paper
+playScore (x, Draw) = signScore x
+
+signScore :: RPS -> Int
+signScore Rock = 1
+signScore Paper = 2
+signScore Scissors = 3
+
+roundScore :: Round -> Int
+roundScore (_,Lose) = 0
+roundScore (_,Draw) = 3
+roundScore (_,Win)  = 6
