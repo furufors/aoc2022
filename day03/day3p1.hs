3f07778ee9f5eb74820d26cd9750623bb4a41bbc
Fredrik Furufors <furufors@gmail.com>
2022-12-03 07:52:17 +0100

    Added day 3

--- day03/day3p1.hs
+++ day03/day3p1.hs
@@ -1,4 +1,22 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+type Rucksack = (String,String)
+
 main :: IO ()
-main = undefined
+main = interact $ show . sum . map (scoreDuplicate . findDuplicate . toRucksack) . lines
+
+toRucksack :: String -> Rucksack
+toRucksack s = let len = length s `div` 2
+               in (take len s, drop len s)
+
+findDuplicate :: Rucksack -> Char
+findDuplicate ((c:cs),s2) = if elem c s2 then c else findDuplicate (cs,s2)
+findDuplicate ([],s2) = error "no duplicate found"
+
+scoreDuplicate :: Char -> Int
+scoreDuplicate c = score c scores
+    where
+        score c1 ((c2,i):ss) = if c1 == c2 then i else score c1 ss
+
+scores :: [(Char,Int)]
+scores = zip ['a'..'z'] [1..26] ++ zip ['A'..'Z'] [27..52]
--- day03/day3p2.hs
+++ day03/day3p2.hs
@@ -1,4 +1,23 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.List
+type Rucksacks = (String,String,String)
+
 main :: IO ()
-main = undefined
+main = interact $ show . sum . map (scoreDuplicate . findDuplicate) . toRucksack . lines
+
+toRucksack :: [String] -> [Rucksacks]
+toRucksack (s1:s2:s3:ss) = (s1,s2,s3):toRucksack ss
+toRucksack ss = []
+
+findDuplicate :: Rucksacks -> Char
+findDuplicate ((c:cs),s2,s3) = if elem c s2 && elem c s3 then c else findDuplicate (cs,s2,s3)
+findDuplicate ([],_,_) = error "no duplicate found"
+
+scoreDuplicate :: Char -> Int
+scoreDuplicate c = score c scores
+    where
+        score c1 ((c2,i):ss) = if c1 == c2 then i else score c1 ss
+
+scores :: [(Char,Int)]
+scores = zip ['a'..'z'] [1..26] ++ zip ['A'..'Z'] [27..52]
