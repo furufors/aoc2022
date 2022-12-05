547f1a26896880e572f57a1578bf3ca5feb672ef
Fredrik Furufors <furufors@gmail.com>
2022-12-05 19:56:55 +0100

    Cleanup day 5 parser

--- day05/day5p1.hs
+++ day05/day5p1.hs
@@ -6,16 +6,20 @@ type Instruction = (Int,Int,Int)
 type Instructions = [Instruction]
 
 main :: IO ()
-main = interact $ show . topLayer . fst . applyInstructions . parse . lines
-
-game = ["VQWMBNZC","BCWRZH","JRQF","TMNFHWSZ","PQNLWFG","WPL","JQCGRDBV","WBNQZ","JTGCFLH"]
-testcase = ["NZ","DCM","P"]
+main = interact $ show . map head . fst . applyInstructions . parse . lines
 
 parse :: [String] -> (Stacks,Instructions)
-parse ss = let first = takeWhile (/="") ss
-               second = drop 10 ss
-               count = maximum . map (read :: String -> Int) . words . last $ first
-           in (game, map parseInstructions second)
+parse ss = let first = init $ takeWhile (/="") ss
+               second = tail $ dropWhile (/="") ss
+               count = maximum . map (read :: String -> Int) . words . last . takeWhile (/="") $ ss
+           in (startingStack count first, map parseInstructions second)
+
+startingStack :: Int -> [String] -> Stacks
+startingStack bins ss =
+    let height = length ss
+        out = [ [char | y <- [0..(height-1)], x < length (ss!!y) , let char = ss!!y!!x, char /= '.']
+                | i <- [1..bins], let x = -3 + 4 * i]
+    in map (dropWhile isSpace) out
 
 parseInstructions :: String -> Instruction
 parseInstructions s = let a = takeWhile isDigit $ drop 5 s
@@ -27,14 +31,10 @@ applyInstructions :: (Stacks,Instructions) -> (Stacks,Instructions)
 applyInstructions (stacks,[]) = (stacks,[])
 applyInstructions (stacks,((m,f,t):rest)) =
     let (letters, s1) = takeFrom m f stacks
-        new = add letters t s1
-    in applyInstructions (new,rest)
+    in applyInstructions (add letters t s1,rest)
 
 takeFrom :: Int -> Int -> Stacks -> (String,[String])
 takeFrom m f ss = (take m (ss !! (f-1)), take (f-1) ss ++ [drop m (ss !! (f-1))] ++ drop f ss)
 
 add :: String -> Int -> Stacks -> Stacks
 add l f ss = take (f-1) ss ++ [reverse l ++ (ss !! (f-1))] ++ drop f ss
-
-topLayer :: Stacks -> String
-topLayer = map head
