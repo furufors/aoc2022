85f9de766667ad3f3813cd3e232f2499b732c7c1
Fredrik Furufors <furufors@gmail.com>
2022-12-05 08:13:52 +0100

    Added day 5

--- day05/day5p2.hs
+++ day05/day5p2.hs
@@ -1,4 +1,40 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.Char
+type Stacks = [String]
+type Instruction = (Int,Int,Int)
+type Instructions = [Instruction]
+
 main :: IO ()
-main = undefined
+main = interact $ show . topLayer . fst . applyInstructions . parse . lines
+
+game = ["VQWMBNZC","BCWRZH","JRQF","TMNFHWSZ","PQNLWFG","WPL","JQCGRDBV","WBNQZ","JTGCFLH"]
+testcase = ["NZ","DCM","P"]
+
+parse :: [String] -> (Stacks,Instructions)
+parse ss = let first = takeWhile (/="") ss
+               second = drop 10 ss
+               count = maximum . map (read :: String -> Int) . words . last $ first
+           in (game, map parseInstructions second)
+
+parseInstructions :: String -> Instruction
+parseInstructions s = let a = takeWhile isDigit $ drop 5 s
+                          b = takeWhile isDigit $ drop 6 $ dropWhile isDigit $ drop 5 s
+                          c = takeWhile isDigit $ drop 4 $ dropWhile isDigit $ drop 6 $ dropWhile isDigit $ drop 5 s
+                      in (read a, read b, read c)
+
+applyInstructions :: (Stacks,Instructions) -> (Stacks,Instructions)
+applyInstructions (stacks,[]) = (stacks,[])
+applyInstructions (stacks,((m,f,t):rest)) =
+    let (letters, s1) = takeFrom m f stacks
+        new = add letters t s1
+    in applyInstructions (new,rest)
+
+takeFrom :: Int -> Int -> Stacks -> (String,[String])
+takeFrom m f ss = (take m (ss !! (f-1)), take (f-1) ss ++ [drop m (ss !! (f-1))] ++ drop f ss)
+
+add :: String -> Int -> Stacks -> Stacks
+add l f ss = take (f-1) ss ++ [l ++ (ss !! (f-1))] ++ drop f ss
+
+topLayer :: Stacks -> String
+topLayer = map head
