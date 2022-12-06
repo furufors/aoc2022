b149d77b97717008a52018807e90e10702f37432
Fredrik Furufors <furufors@gmail.com>
2022-12-06 07:39:17 +0100

    Added day 6

--- day06/day6p1.hs
+++ day06/day6p1.hs
@@ -1,4 +1,12 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.List
 main :: IO ()
-main = undefined
+main = interact $ show . indexOfMarker 0 . head . lines
+
+indexOfMarker :: Int -> String -> Int
+indexOfMarker i ss = let chars =  [ss!!k | j <- [0..3], let k = i + j]
+                         uniq = nub $ sort chars
+                         lenUniq = length  uniq
+                         len = length chars
+                     in if lenUniq == 4 then i + 4 else indexOfMarker (i+1) ss
--- day06/day6p2.hs
+++ day06/day6p2.hs
@@ -1,4 +1,12 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.List
 main :: IO ()
-main = undefined
+main = interact $ show . indexOfMarker 0 . head . lines
+
+indexOfMarker :: Int -> String -> Int
+indexOfMarker i ss = let chars =  [ss!!k | j <- [0..13], let k = i + j]
+                         uniq = nub $ sort chars
+                         lenUniq = length  uniq
+                         len = length chars
+                     in if lenUniq == 14 then i + 14 else indexOfMarker (i+1) ss
