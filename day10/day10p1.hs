ae1f04f724085c4bf6fb385718a5a997b827b692
Fredrik Furufors <furufors@gmail.com>
2022-12-10 08:34:10 +0100

    Added day 10

--- day10/day10p1.hs
+++ day10/day10p1.hs
@@ -1,4 +1,20 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+data Instruction = Add Int | Noop deriving (Show)
+
 main :: IO ()
-main = undefined
+main = interact $ show . sum . numbers . execute [] . map parse . lines
+
+numbers :: [Int] -> [Int]
+numbers is = [i * is !! (i-2) | i <- [20,60,100,140,180,220]]
+
+execute :: [Int] -> [Instruction] -> [Int]
+execute [] (Noop:is) = execute [1] is
+execute [] ((Add x):is) = execute [1, 1+x] is
+execute as [] = as
+execute as (Noop:is)    = execute (as ++ [last as]) is
+execute as ((Add x):is) = execute (as ++ [last as, last as + x]) is
+
+parse :: String -> Instruction
+parse "noop" = Noop
+parse x = Add . read . tail $ dropWhile (/=' ') x
--- day10/day10p2.hs
+++ day10/day10p2.hs
@@ -1,4 +1,27 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.List
+import Data.List.Split
+data Instruction = Add Int | Noop deriving (Show)
+
 main :: IO ()
-main = undefined
+main = interact $ intercalate "\n" . toScreen . execute [] . map parse . lines
+
+toScreen :: [Int] -> [String]
+toScreen is = chunksOf 40 . map visible $ zip is [1..]
+
+visible (p,i) = if p - (i `mod` 40) `elem` [-1,0,1] then '#' else '.'
+
+numbers :: [Int] -> [Int]
+numbers is = [i * is !! (i-2) | i <- [20,60,100,140,180,220]]
+
+execute :: [Int] -> [Instruction] -> [Int]
+execute [] (Noop:is) = execute [1] is
+execute [] ((Add x):is) = execute [1, 1+x] is
+execute as [] = as
+execute as (Noop:is)    = execute (as ++ [last as]) is
+execute as ((Add x):is) = execute (as ++ [last as, last as + x]) is
+
+parse :: String -> Instruction
+parse "noop" = Noop
+parse x = Add . read . tail $ dropWhile (/=' ') x
