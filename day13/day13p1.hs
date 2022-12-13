759eea83f37248c7e3584bb219b259567a86a311
Fredrik Furufors <furufors@gmail.com>
2022-12-13 14:37:49 +0100

    Cleanup day 13

--- day13/day13p1.hs
+++ day13/day13p1.hs
@@ -3,9 +3,10 @@
 import Text.Parsec
 data Packet = Val Int | Group [Packet] deriving Show
 type PacketPair = (Packet,Packet)
+
 main :: IO ()
 main = interact $ show . score 1 . map comparePacketPair . parsein . lines
---score 1 .
+
 score :: Int -> [Ordering] -> Int
 score i [] = 0
 score i (LT:rest) = i + score (i+1) rest
@@ -25,19 +26,7 @@ comparePacketPair (Group as, Group bs) = case comparePacketPair (head as, head b
         LT -> LT
         GT -> GT
         EQ -> comparePacketPair (Group (tail as),Group (tail bs))
--- [[1],[2,3,4]] vs [[1],4]
-{-
-If both values are integers, the lower integer should come first.
-    If the left integer is lower than the right integer, the inputs are in the right order.
-    If the left integer is higher than the right integer, the inputs are not in the right order.
-    Otherwise, the inputs are the same integer; continue checking the next part of the input.
-If both values are lists, compare the first value of each list, then the second value, and so on.
-    If the left list runs out of items first, the inputs are in the right order.
-    If the right list runs out of items first, the inputs are not in the right order.
-    If the lists are the same length and no comparison makes a decision about the order, continue checking the next part of the input.
-If exactly one value is an integer, convert the integer to a list which contains that integer as its only value, then retry the comparison.
-    For example, if comparing [0,0,0] and 2, convert the right value to [2] (a list containing 2); the result is then found by instead comparing [0,0,0] and [2].
- -}
+
 parsein :: [String] -> [PacketPair]
 parsein [        ] = []
 parsein ("":ss) = parsein ss
