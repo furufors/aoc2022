43fc111a8d1f28279740b99f17aa6e8abdae9e00
Fredrik Furufors <furufors@gmail.com>
2022-12-15 11:03:49 +0100

    Added day 15

--- day15/day15p1.hs
+++ day15/day15p1.hs
@@ -1,4 +1,39 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Text.Parsec
+type Pos = (Int,Int)
+data Data = Data (Pos,Pos) deriving (Show, Eq) -- (Sensor, Beacon)
+
 main :: IO ()
-main = undefined
+main = interact $ show . length . filter id . exclusionsY 2000000 . map parsein . lines
+offset = 1000000
+
+exclusionsY :: Int -> [Data] -> [Bool]
+exclusionsY y ds =
+    let maxx = offset + (maximum $ map (\(Data ((a,b),(c,d))) -> max a c) ds)
+        minx = -offset + (minimum $ map (\(Data ((a,b),(c,d))) -> min a c) ds)
+    in [ any id [manhattan (x,y) (a,b) <= manhattan (a,b) (c,d) && (x,y) /= (c,d)| (Data ((a,b),(c,d))) <- ds] | x <- [minx..maxx]]
+
+manhattan :: Pos -> Pos -> Int
+manhattan (a,b) (c,d) = abs (a-c) + abs (b-d)
+
+parsein :: String -> Data
+parsein input = case parse (try sensorData) "parsein" input of
+    Left err -> error $ show err
+    Right a -> a
+
+sensorData :: Parsec String () Data
+sensorData = do
+    _ <- string "Sensor at x="
+    a <- parseInt
+    _ <- string ", y="
+    b <- parseInt
+    _ <- string ": closest beacon is at x="
+    c <- parseInt
+    _ <- string ", y="
+    d <- parseInt
+    return $ Data ((a,b),(c,d))
+
+parseInt = parseIntN <|> parseIntP
+parseIntN = string "-" >> parseInt >>= (\s -> return $ (-1) * s)
+parseIntP =  read <$> many1 digit
--- day15/day15p2.hs
+++ day15/day15p2.hs
@@ -1,4 +1,55 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.List
+import Text.Parsec
+type Pos = (Int,Int)
+data Data = Data (Pos,Pos) deriving (Show, Eq) -- (Sensor, Beacon)
+data Exclusion = Exclusion (Int,Pos,Pos)
+
 main :: IO ()
-main = undefined
+main = interact $ show . exclusions . toExclusion . map parsein . lines
+offset = 1000000
+upLim = 4000000
+
+toExclusion :: [Data] -> [Exclusion]
+toExclusion = reverse . sortBy maxManhattan . map (\(Data (a,b)) -> Exclusion (manhattan a b, a, b))
+
+maxManhattan (Exclusion (a,_,_)) (Exclusion (b,_,_)) = compare a b
+
+exclusionsY :: Int -> [Exclusion] -> [(Int,Int)]
+exclusionsY y es = [ (a-dx,a+dx) | (Exclusion (d,(a,b),_)) <- es, let dx = max 0 (1+d-(abs (y-b))) ]
+exclusionsX :: Int -> [Exclusion] -> [(Int,Int)]
+exclusionsX x es = [ (b-dy,b+dy) | (Exclusion (d,(a,b),_)) <- es, let dy = max 0 (1+d-(abs (x-a))) ]
+
+
+exclusions :: [Exclusion] -> Int
+exclusions ds =
+    let exy = \y -> exclusionsY y ds
+        exx = \x -> exclusionsX x ds
+    in head [ x * 4000000 + y | x <- [0..upLim], y <- [0..upLim], all (not . inside x) (exy y) && all (not . inside y) (exx x) ]
+
+manhattan :: Pos -> Pos -> Int
+manhattan (a,b) (c,d) = abs (a-c) + abs (b-d)
+
+inside x (a,b) = a < x && b > x
+
+parsein :: String -> Data
+parsein input = case parse (try sensorData) "parsein" input of
+    Left err -> error $ show err
+    Right a -> a
+
+sensorData :: Parsec String () Data
+sensorData = do
+    _ <- string "Sensor at x="
+    a <- parseInt
+    _ <- string ", y="
+    b <- parseInt
+    _ <- string ": closest beacon is at x="
+    c <- parseInt
+    _ <- string ", y="
+    d <- parseInt
+    return $ Data ((a,b),(c,d))
+
+parseInt = parseIntN <|> parseIntP
+parseIntN = string "-" >> parseInt >>= (\s -> return $ (-1) * s)
+parseIntP =  read <$> many1 digit
