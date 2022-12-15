553c3cc0b25b845199c3bbae3e9726d6fe234a2e
Fredrik Furufors <furufors@gmail.com>
2022-12-15 14:58:51 +0100

    Added day 15

--- day15/day15p2.hs
+++ day15/day15p2.hs
@@ -1,7 +1,9 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
 import Data.List
+import Data.Ord
 import Text.Parsec
+import Debug.Trace
 type Pos = (Int,Int)
 data Data = Data (Pos,Pos) deriving (Show, Eq) -- (Sensor, Beacon)
 data Exclusion = Exclusion (Int,Pos,Pos)
@@ -21,12 +23,31 @@ exclusionsY y es = [ (a-dx,a+dx) | (Exclusion (d,(a,b),_)) <- es, let dx = max 0
 exclusionsX :: Int -> [Exclusion] -> [(Int,Int)]
 exclusionsX x es = [ (b-dy,b+dy) | (Exclusion (d,(a,b),_)) <- es, let dy = max 0 (1+d-(abs (x-a))) ]
 
-
 exclusions :: [Exclusion] -> Int
 exclusions ds =
     let exy = \y -> exclusionsY y ds
         exx = \x -> exclusionsX x ds
-    in head [ x * 4000000 + y | x <- [0..upLim], y <- [0..upLim], all (not . inside x) (exy y) && all (not . inside y) (exx x) ]
+    in head [ x * 4000000 + y | y <- [0..upLim], let ranges = consolidated $ exy y, not (singleLarge ranges), x <- [0..upLim], all (not . inside x) (ranges)]
+
+consolidated :: [(Int, Int)] -> [(Int, Int)]
+consolidated = converge (foldr go [] . sort . fmap ab)
+  where
+    go xy [] = [xy]
+    go xy@(x, y) abetc@((a, b) : etc)
+      | y > b = xy : etc
+      | y > a = (x, b) : etc
+      | otherwise = xy : abetc
+    ab (a, b)
+      | a < b = (a, b)
+      | otherwise = (b, a)
+
+converge :: Eq a => (a -> a) -> a -> a
+converge f a = let a' = f a in if a == a' then a else converge f a'
+
+singleLarge :: [(Int, Int)] -> Bool
+singleLarge [] = False
+singleLarge [(a,b)] | a <= 0 && b >= upLim = True
+singleLarge a = False
 
 manhattan :: Pos -> Pos -> Int
 manhattan (a,b) (c,d) = abs (a-c) + abs (b-d)
