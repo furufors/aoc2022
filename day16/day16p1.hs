97ec74e516399c99f913a6f7f7930116e868d02c
Fredrik Furufors <furufors@gmail.com>
2022-12-16 13:52:56 +0100

    WIP

--- day16/day16p1.hs
+++ day16/day16p1.hs
@@ -4,16 +4,21 @@ import Text.Parsec
 import Algorithm.Search
 import qualified Data.Map as M
 import Data.Maybe
+import Data.List
 import Debug.Trace
 data Valve = Valve (String,Int,[String]) deriving (Show, Eq)
 data Link = Link (String,Int,Int,String) deriving (Show, Eq) -- (Start,Flow,Dist,End)
 
 toLinks :: [Valve] -> [Link]
-toLinks all = toLinks' all
+toLinks all = filter (remzero) $ toLinks' all
     where
         nonZeros = filter (not . zeroFlow) all
         toLinks' (v:vs) = (concat . map (shortestPath v all) $ filter (not . sameName v) nonZeros) ++ toLinks' vs
         toLinks' [] = []
+
+remzero (Link ("AA",_,_,_)) = True
+remzero (Link (_,0,_,_)) = False
+remzero a = True
 
 shortestPath :: Valve -> [Valve] -> Valve -> [Link]
 shortestPath (Valve (s1,f1,ss1)) vs (Valve (s2,f2,ss2)) =
@@ -36,24 +41,26 @@ sameName :: Valve -> Valve -> Bool
 sameName (Valve (a,_,_)) (Valve (b,_,_)) = a == b
 
 main :: IO ()
-main = interact $ show . toLinks . map parseValve . lines
+main = interact $ show . solve . toLinks . map parseValve . lines
 
-solve :: [Valve] -> Maybe (Int, [(Int, Int, Int, M.Map String Bool, String)])
+solve :: [Link] -> Maybe (Int, [(Int, Int, Int, M.Map String Bool, String)])
 solve vs =
     let start = (30, 0, 0, setClosed vs,"AA")
+        setClosed [                  ] = M.empty
+        setClosed ((Link (s,_,_,_)):rest) = M.insert s False $ setClosed rest
     in dijkstra next cost end start
         where
-            next state = openValve state ++ moveDirectly state
-            cost (i1,f1,d1,_,_) (i2,f2,d2,_,_) = 10000-f2
-            end (i,_,_,_,_) = i == 0
-            setClosed [                  ] = M.empty
-            setClosed ((Valve (s,_,_)):rest) = M.insert s False $ setClosed rest
-            openValve (i,f,d,valveStatus, pos) = if newFlow 0 0 pos == 0 || (fromJust $ M.lookup pos valveStatus) then [] else [(i-1, newFlow f i pos, d-(newFlow f i pos), M.insert pos True valveStatus, pos)]
-            moveDirectly (i,f,d,valveStatus,pos) = let (Valve (_,_,ss)) = getValve pos in map (\s -> (i-1, f, d-f, valveStatus,s)) ss
-            newFlow f i pos = let (Valve (_,nf,ss)) = getValve pos in f + nf
+            next state = openValve state ++ moveDirectly state -- ++ wait state
+            wait (i,f,d,valveStatus, pos) = [(i+1,f,d-f,valveStatus, pos)]
+            cost (i1,f1,d1,_,_) (i2,f2,d2,_,_) = let im = max 0 i2 in -1 * f2 * (i1-im) + 10000*(i1-im)
+            end (i,_,_,_,_) = i <= 0
+            openValve (i,f,d,valveStatus, pos) = if newFlow 0 0 pos == 0 || (fromJust $ M.lookup pos valveStatus) then [] else [(i-1, newFlow f i pos, d - f, M.insert pos True valveStatus, pos)]
+            moveDirectly (i,f,d,valveStatus,pos) = let next = nub $ filter (from pos) vs in map (\(Link (_,_,dist,s)) -> (i-dist, f, d-(f*dist), valveStatus,s)) $ next
+            from s l@(Link (a,_,_,_)) = s == a
+            newFlow f i pos = let (Link (_,nf,_,_)) = getValve pos in f + nf
             getValve pos = getValve' vs pos
             getValve' [] _ = error "Couldn't find valve"
-            getValve' (v@(Valve (es,f,ss)):rest) cs = if es == cs then v else getValve' rest cs
+            getValve' (v@(Link (es,f,dist,ss)):rest) cs = if es == cs then v else getValve' rest cs
 
 parseValve :: String -> Valve
 parseValve input = case parse (try valve) "parsein" input of
