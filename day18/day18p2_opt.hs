f7eeee494d02dfdfa2ab065c4bd9d0826f71b154
Fredrik Furufors <furufors@gmail.com>
2022-12-18 18:27:16 +0100

    Improved parallelized solution

--- day18/day18p2_opt.hs
+++ day18/day18p2_opt.hs
@@ -13,7 +13,7 @@ main = timeIt $ interact $ show . compute . parMap rpar parseRock . lines
 
 compute rocks =
     let roxette = Set.fromList rocks
-        minmax acc = let ps = map acc rocks in (minimum ps - 1, maximum ps + 1)
+        minmax acc = let ps = parMap rpar acc rocks in (minimum ps - 1, maximum ps + 1)
         (minx,maxx) = minmax (\(x,_,_) -> x)
         (miny,maxy) = minmax (\(_,y,_) -> y)
         (minz,maxz) = minmax (\(_,_,z) -> z)
@@ -27,8 +27,9 @@ compute rocks =
                 newNodes = concat $ map fst updates
                 newSides = sum $ map snd updates
                 update new = if Set.member new roxette then ([], 1) else ([new],0)
-            in visit (newNodes ++ ns) (Set.union (Set.fromList newNodes) visited) (sideSeen + newSides)
-    in visit [(minx,miny,minz)] (Set.singleton (minx,miny,minz)) 0
+                visited' = Set.union (Set.fromList newNodes) visited
+            in newNeighbours `seq` updates `seq` ((newNodes `seq` visited') `par` newSides) `seq` visit (newNodes ++ ns) visited' (sideSeen + newSides)
+    in roxette `par` minx `par` maxx `par` miny `par` maxy `par` minz `par`maxz `seq` visit [(minx,miny,minz)] (Set.singleton (minx,miny,minz)) 0
 
 parseRock :: String -> Rock
 parseRock input = case parse rock "parsein" input of
