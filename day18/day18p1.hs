baa1d28a5724c127041b3d0732e42fa990897b0d
Fredrik Furufors <furufors@gmail.com>
2022-12-18 13:14:35 +0100

    Added day 18

--- day18/day18p1.hs
+++ day18/day18p1.hs
@@ -1,4 +1,30 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Text.Parsec
+type Rock = (Int,Int,Int)
 main :: IO ()
-main = undefined
+main = interact $ show . countSides . map parseRock . lines
+
+countSides rocks = (6 * length rocks) - sum (adjacent rocks)
+adjacent [] = []
+adjacent ((x,y,z):rocks) = [if adjPair (abs (a-x)) (abs (b-y)) (abs (c-z)) == 1 then 2 else 0
+                 | (a,b,c) <- rocks ] ++ (adjacent rocks)
+
+adjPair 1 0 0 = 1
+adjPair 0 1 0 = 1
+adjPair 0 0 1 = 1
+adjPair _ _ _ = 0
+
+parseRock :: String -> Rock
+parseRock input = case parse (try rock) "parsein" input of
+    Left err -> error $ show err
+    Right a -> a
+
+rock :: Parsec String () Rock
+rock = do
+    x  <- read <$> many1 digit
+    _  <- string ","
+    y  <- read <$> many1 digit
+    _  <- string ","
+    z  <- read <$> many1 digit
+    return $ (x,y,z)
--- day18/day18p2.hs
+++ day18/day18p2.hs
@@ -1,4 +1,39 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Text.Parsec
+import Data.Set (Set)
+import qualified Data.Set as Set
+type Rock = (Int,Int,Int)
+
 main :: IO ()
-main = undefined
+main = interact $ show . compute . map parseRock . lines
+
+compute rocks =
+    let minp = minimum (map (\(x,y,z) -> minimum [x,y,z]) rocks) - 1
+        maxp = maximum (map (\(x,y,z) -> maximum [x,y,z]) rocks) + 1
+        neighbours (x,y,z) = Set.fromList $ filter insideBounds [(x-1,y,z),(x+1,y,z),(x,y-1,z),(x,y+1,z),(x,y,z-1),(x,y,z+1)]
+        insideBounds (x,y,z) = all (\p -> minp <= p && p <= maxp) [x,y,z]
+        visit :: [Rock] -> Set Rock -> Int -> Int
+        visit [] visited sideSeen = sideSeen
+        visit (n:ns) visited sideSeen =
+            let newNeighbours = neighbours n Set.\\ visited
+                (ns', sideSeen', visited') = foldl update (ns,sideSeen,visited) (Set.toList newNeighbours)
+                update (ns, sideSeen, visited) new =
+                    if new `elem` rocks
+                    then (ns, sideSeen + 1, visited)
+                    else (new:ns,sideSeen, Set.insert new visited)
+            in visit ns' visited' sideSeen'
+    in visit [(minp,minp,minp)] (Set.singleton (minp,minp,minp)) 0
+
+parseRock :: String -> Rock
+parseRock input = case parse (try rock) "parsein" input of
+    Left err -> error $ show err
+    Right a -> a
+
+rock = do
+    x  <- read <$> many1 digit
+    _  <- string ","
+    y  <- read <$> many1 digit
+    _  <- string ","
+    z  <- read <$> many1 digit
+    return $ (x,y,z)
